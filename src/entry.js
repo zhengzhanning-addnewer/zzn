cb8900ebc91867ba9b147af6123bef454936d872
zhengzhanning <zhengzhanning@addnewer.com>
2016-07-06 18:36:08 +0800

    fwef

--- src/entry.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('app').innerHTML="这是我第一个打包成功的程序";
-//添加
-require("./main.js");
-require("./style/style.css");
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,128 @@
-var Webpack = require("webpack");
-var path = require("path");
-module.exports = {
-    entry: ["./src/entry.js"],
+'use strict';
+
+var webpack = require("webpack");
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
+
+var config = {
     output: {
-        path: __dirname,
-        filename: "bundle.js"
+        path: path.join (__dirname + '/public'),
+        filename: "[name].js",
+        chunkFilename: '[id].chunk.js',
+        //publicPath: path.join (__dirname+ "public")
+    },
+    entry:{
+        comm: ['jquery','angular','angular-ui-router','angular-animate','moment'],
+        //app: ['webpack/hot/dev-server', path.join(__dirname, '/src/js/app.js')]
+        app: './src/js/app.js'
+    },
+    resolve: {
+        modulesDirectories: ['node_modules'],
+        alias: {
+            'npm': __dirname + '/node_modules',
+        },
+        extensions:['', '.js','.html', '.css', '.styl']
     },
     module: {
-        loaders: [{
-                test: /\.css$/,
-                loader: "style!css"
+        noParse:[
+            path.join(__dirname+'/node_modules', '/video.js/dist/video.js')
+        ],
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel',
+                exclude: /node_modules/
             },
-            { test: /\.(png|jpg)$/, loader: "url-loader?limit=8192" } // 添加到这！并且会按照文件大小, 或者转化为 base64, 或者单独作为文件
-            //在大小限制后可以加上&name=./build/images/[name]ys.[ext]，会将我们的文件生成在设定的文件夹下。
+            //style-loader!css-loader
+            { test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader") },
+            { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192'},
+            {
+                test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url?limit=10000&minetype=application/font-woff'
+            },
+            {
+                test: /\.woff2$/,
+                loader: 'url?limit=10000&minetype=application/font-woff'
+            },
+            {
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url?limit=10000&minetype=application/octet-stream'
+            },
+            {
+                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'file'
+            },
+            {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url?limit=10000&minetype=image/svg+xml'
+            },
+            {
+                test: /\.styl$/,
+                loader: ExtractTextPlugin.extract(
+                    'style',
+                    "css?sourceMap!stylus?sourceMap"
+                )
+            }
         ]
     },
+    babel: {
+        presets: ['es2015'],
+        plugins: ['transform-runtime']
+    },
+    devtool: 'eval',
     plugins: [
-        new Webpack.BannerPlugin("这里是打包文件头部注释！") //注意这是一个数组..
-    ]
-}
+        new webpack.optimize.CommonsChunkPlugin({
+            name:'comm',
+            filename:'comm.js'
+        }),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery' : 'jquery'
+        }),
+        new ExtractTextPlugin('index.css'),
+        new HtmlWebpackPlugin({
+            template: './views/index.html',
+            filename: './index.html',
+            inject: true
+        }),
+        new webpack.DefinePlugin({
+            DEV: true,
+        })
+    ],
+    devServer: {
+        port: 3131,
+        contentBase: __dirname,
+        proxy:{
+            '/mock/admin/v1*':{
+                target: 'http://192.168.0.30/mockjsdata/5',
+                rewrite : function(req){
+                    req.url = req.url.replace(/^\/mock/,'');
+                    console.log(req.url);
+                },
+                changeOrigin: true,
+                secure: false
+            },
+            '/management*':{
+                target: 'http://192.168.0.30/mockjsdata/11',
+                rewrite : function(req){
+                    req.url = req.url.replace(/^\/management/,'');
+                    console.log(req.url);
+                },
+                changeOrigin: true,
+                secure: false
+            },
+            '/dev*':{
+                target: 'http://ssp.test',
+                rewrite : function(req){
+                    req.url = req.url.replace(/^\/dev/,'');
+                },
+                changeOrigin: true,
+                secure: false
+            }
+        }
+    }
+};
+
+module.exports = config;
