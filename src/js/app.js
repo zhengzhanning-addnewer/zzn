d8d93ac3720800290530100d8691f07bfb8b24c1
zhengzhanning <zhengzhanning@addnewer.com>
2016-08-01 15:05:32 +0800

    ef

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,36 @@
+'use strict';
 
-document.write('wefjwioefjwe');
+/**
+ * @Date 2016-07-07
+ * @auther asdf
+ *
+ * angularJS配置文件
+ * 路由：angular ui-router
+ *
+ */
+//样式
+import '../css/index.styl';
+import '../../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../../node_modules/angular-ui-bootstrap/dist/ui-bootstrap-csp.css';
+//angular
+import 'angular';
+import 'angular-ui-router';
+import './service';
+import './dreck';
+let app = angular.module('app', [
+    'ui.router',
+    'service',
+    'dreck'
+    ]);
+
+app.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider,$locationProvider) {
+    $urlRouterProvider.when('','/content.html');
+    // For any unmatched url, redirect to /state1
+    $urlRouterProvider.otherwise("/content.html");
+    
+    $locationProvider.html5Mode([true]);
+}]);
+
+export default app;
+
+require('./index')(app);
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 'use strict';
-
+var fs = require("fs") ;
 var webpack = require("webpack");
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var HtmlWebpackPlugin = require('html-webpack-plugin');
